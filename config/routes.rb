5b8ec0e8671d78feb965d0cc5e07d96c408c2323
alexandre grenier <alexandre18grenier@hotmail.com>
2017-05-17 10:56:42 +0200

    Exercise 2 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+
+  # Afficher tous les articles
+  # get 'articles', to: 'articles#index'
+  # # Afficher un article
+  # get 'articles/:id', to: 'articles#show'
+  # # Ajouter un article
+  # get 'articles/new', to: 'articles#new'
+  # post 'articles', to: 'articles#create'
+  # # Updater un article
+  # get 'articles/:id/edit', to: 'articles#edit'
+  # patch 'articles/:id', to: 'articles#update'
+  # # Détruire un article
+  # delete 'articles/:id', to: 'articles#destroy'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 10.times do |article|
+#   Article.create!(title: 'Faker::StarWars.character', content: 'Faker::StarWars.quote')
+# end
+
+# Article.create!(title: 'Anakin', content: 'The force is with me')
+# Article.create!(title: 'Dark vador', content: 'I am your father')
